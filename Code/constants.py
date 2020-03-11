3bbb61cf94520db3f3b8faa25a7da0af3157bc8b
Cordes-RJ <richardj.cordes@gmail.com>
2020-03-10 23:36:56 -0400

    renamed file

--- Code/constants.py
+++ Code/constants.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 sitelistpath = "C:/Users/XYZ/Google Drive/DocLib/COGSEC/Metascrape/SiteList.csv"
+sitedatapath = "C:/Users/XYZ/Google Drive/DocLib/COGSEC/Metascrape/SiteData.csv"
