b7a0d61737d229c5a54bbb98eed6acb7d82db82e
Cordes-RJ <richardj.cordes@gmail.com>
2020-03-11 10:40:26 -0400

    added loop, so that manager runs continuously

--- Code/Mr.py
+++ Code/Mr.py
@@ -4,15 +4,26 @@ import Constants
 import SiteList
 import rankTrafficScrape as rts
 import SiteData
+import listener
 
 class Manager:
     def __init__(self):
-        pass
+        self.Run()
     def Delegate(self):
+        print("> Getting Site List...")
         sitelist = SiteList.GetSiteList(Constants.sitelistpath)
+        print("> Scraping Politely...")
         DataList = rts.Scraper().Scrape(sitelist,"Just testing some stuff for research purposes--trying to be as polite as possible--contact at rj.cordes@cogsec.org", 4)
+        print("> Writing SiteData to CSV...")
         SiteData.writeToSiteData(DataList)
+        print(" > Cleaning Up...")
         SiteList.EmptyContents(Constants.sitelistpath)
-    
-#%%
-m = Manager().Delegate()
+    def Run(self):
+        EAR = listener.listener(Constants.sitelistpath, 30)
+        while True:
+            if EAR.listen():
+                print("> Running Scrape...")
+                self.Delegate()
+                print("> Scrape Concluded")
+            else:
+                EAR.WaitAndListen()
