b3206ef32ecdb346078548056d5b194c9a1dbde9
Cordes-RJ <richardj.cordes@gmail.com>
2020-03-11 10:44:04 -0400

    updated

--- Code/constants.py
+++ Code/constants.py
@@ -2,3 +2,4 @@
 
 sitelistpath = "C:/Users/XYZ/Google Drive/DocLib/COGSEC/Metascrape/SiteList.csv"
 sitedatapath = "C:/Users/XYZ/Google Drive/DocLib/COGSEC/Metascrape/SiteData.csv"
+checkHTML = "failedToParse.html"
